b2a53aa1e12fe30f5616cbd5844c264820d658ca
prashanthm2410 <97015668+prashanthm2410@users.noreply.github.com>
2023-07-13 18:24:04 +0530

    Create GREP.c

---
+++ GREP.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main(int argc,char *argv[])
+{
+	if(argc<1)
+	{
+	printf("File Not Passed\n");
+	exit(1);
+	}
+	char ch;
+	printf("Enter the Pattern to Be searched  : ");
+	char pat[15],temp[15];scanf("%s",pat);
+	FILE *fp = fopen(argv[1],"r");
+	while(!feof(fp))
+	{
+	fgets(temp,100,fp);
+	if(strstr(temp,pat))
+	printf("Pattern Matched");
+	else
+	printf("Pattern NOT Matched");
+	}
+}
