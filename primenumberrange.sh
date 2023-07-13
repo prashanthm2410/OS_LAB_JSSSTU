a37d9bbf85967ce1ae8dc3f8d20b266b5a4ec564
prashanthm2410 <97015668+prashanthm2410@users.noreply.github.com>
2023-07-13 18:18:56 +0530

    Create primenumberrange.sh

---
+++ primenumberrange.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter the Number for The Range: "
+read n
+echo "The Prime Numbers between the given range are:"
+echo
+
+flag=0
+for ((i=2; i<=$n; i++))
+do
+    flag=0
+    for ((j=2; j<=i/2; j++))
+    do
+        if [ $((i%j)) -eq 0 ]; then
+            flag=1
+            break
+        fi
+    done
+    if [ $flag -eq 0 ]; then
+        echo -ne "$i "
+    fi
+done
