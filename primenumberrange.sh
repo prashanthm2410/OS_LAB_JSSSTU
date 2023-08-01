cd8d831264ce8f80985fe586021e3f3d27db1cdc
prashanthm2410 <97015668+prashanthm2410@users.noreply.github.com>
2023-08-01 09:57:55 +0530

    Update primenumberrange.sh

--- primenumberrange.sh
+++ primenumberrange.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
-
-echo "Enter the Number for The Range: "
-read n
-echo "The Prime Numbers between the given range are:"
-echo
-
-flag=0
-for ((i=2; i<=$n; i++))
-do
-    flag=0
-    for ((j=2; j<=i/2; j++))
-    do
-        if [ $((i%j)) -eq 0 ]; then
-            flag=1
-            break
-        fi
-    done
-    if [ $flag -eq 0 ]; then
-        echo -ne "$i "
-    fi
-done
+#!/bin/bash 
+  
+ echo "Enter the Numbers for The Range: " 
+ read m 
+ read n
+ echo "The Prime Numbers between the given range are:" 
+ echo 
+  
+ flag=0 
+ for ((i=$m; i<=$n; i++)) 
+ do 
+     flag=0 
+     for ((j=2; j<=i/2; j++)) 
+     do 
+         if [ $((i%j)) -eq 0 ]; then 
+             flag=1 
+             break 
+         fi 
+     done 
+     if [ $flag -eq 0 ]; then 
+         echo -ne "$i " 
+     fi 
+ done
